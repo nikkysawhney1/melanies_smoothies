bbc858ef293a087fb2f35e7150ae42c72d770810
nikkysawhney1 <nikky.sawhney@gmail.com>
2025-07-17 17:43:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
         
 #new section to display smootie nutrition info
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
